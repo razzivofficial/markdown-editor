56eeb6f828a3260fb7415c52a30a17a7a619daed
razzivofficial <razzivofficial@gmail.com>
2023-09-22 10:41:34 +0530

    adding-working-markdown-compiler

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import MarkdownEditor from "@uiw/react-markdown-editor";
+import "@wcj/dark-mode";
+import { useState } from "react";
+import "./App.css";
+
+const code = `#hi this is working`;
+
+export default function App() {
+  const [markdownVal, setMarkdownVal] = useState(code);
+  console.log("markdownVal:", markdownVal);
+  return (
+    <div>
+      <dark-mode light="Light" dark="Dark"></dark-mode>
+      <h3>Auto</h3>
+      <div className="App">
+        <MarkdownEditor
+          value={markdownVal}
+          onChange={(value) => {
+            setMarkdownVal(value);
+          }}
+        />
+      </div>
+      <h3>Light</h3>
+      <div className="App" data-color-mode="light">
+        <MarkdownEditor
+          value={markdownVal}
+          onChange={(value) => {
+            setMarkdownVal(value);
+          }}
+        />
+      </div>
+      <h3>Dark</h3>
+      <div className="App" data-color-mode="dark">
+        <MarkdownEditor
+          value={markdownVal}
+          onChange={(value) => {
+            setMarkdownVal(value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
